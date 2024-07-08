25fdbdf9f33540f2181371cf7ca40cf7bfcb7cf2
misaoi <146754435+misaoi@users.noreply.github.com>
2024-07-09 02:18:59 +0800

    Update youtube_to_m3u.py

--- yt_to_m3u8/yt_to_m3u8/youtube_to_m3u.py
+++ yt_to_m3u8/yt_to_m3u8/youtube_to_m3u.py
@@ -1,13 +1,14 @@
 import requests
 import pandas as pd
 data = pd.read_csv("youtube_channel.csv")
+i = 0
 last_id = data['id'].iloc[-1]
-for i in range(last_id+1):
+for i in range(1,last_id+1):
   channel_name = data[data['id'] == i]['channel_name'].values[0]
   url = data[data['id'] == i]['url'].values[0]
   response = requests.get(url, timeout=15).text
   if '.m3u8' not in response:
-    m3u8link = "https://raw.githubusercontent.com/misaoi/tv/main/yt_to_m3u8/yt_to_m3u8/nolive/index.m3u8"
+    m3u8link = "https://raw.githubusercontent.com/GaryZong/YouTube_to_m3u/main/scripts/nolive/index.m3u8"
   else:
     end = response.find('.m3u8') + 5
     tuner = 100
@@ -26,5 +27,3 @@ for i in range(last_id+1):
     f = open("m3u_list/"+ channel_name + ".m3u8", 'w')
     f.write(m3u8_1 + '\n' + m3u8_2 + '\n' + m3u8_3+ '\n' + m3u8link)
     f.close()
-    print(channel_name+"\n")
-    print(m3u8link+"\n")
