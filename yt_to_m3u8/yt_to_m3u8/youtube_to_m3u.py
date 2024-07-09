a0713b757ff780904febc7206711c83d168a0817
misaoi <146754435+misaoi@users.noreply.github.com>
2024-07-09 20:40:53 +0800

    Update youtube_to_m3u.py

--- yt_to_m3u8/yt_to_m3u8/youtube_to_m3u.py
+++ yt_to_m3u8/yt_to_m3u8/youtube_to_m3u.py
@@ -3,8 +3,8 @@ import pandas as pd
 data = pd.read_csv("youtube_channel.csv")
 i = 0
 last_id = data['id'].iloc[-1]
-for i in range(1,last_id+1):
-  channel_name = data[data['id'] == i]['channel_name'].values[0]
+for i in range(last_id+1):
+  file_name = data[data['id'] == i]['file_name'].values[0]
   url = data[data['id'] == i]['url'].values[0]
   response = requests.get(url, timeout=15).text
   if '.m3u8' not in response:
@@ -24,6 +24,9 @@ for i in range(1,last_id+1):
     m3u8_1 = "#EXTM3U"
     m3u8_2 = "#EXT-X-VERSION:3"
     m3u8_3 = "#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=540000"
-    f = open("m3u_list/"+ channel_name + ".m3u8", 'w')
+    f = open("m3u_list/"+ file_name + ".m3u8", 'w')
     f.write(m3u8_1 + '\n' + m3u8_2 + '\n' + m3u8_3+ '\n' + m3u8link)
     f.close()
+
+    print(file_name+"\n")
+    print(m3u8link+"\n")
